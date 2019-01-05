6156820ebb027205689e81f552437b94bb50726d
Sitthinon Chanaritthichai <31182493+FighttoSitthinon@users.noreply.github.com>
2019-01-05 23:13:59 +0700

    Update work exp.

--- MyWebApp/ClientApp/src/app/work-exp/work-exp.component.ts
+++ MyWebApp/ClientApp/src/app/work-exp/work-exp.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-workExp',
   templateUrl: './work-exp.component.html',
+  styleUrls: ['./work-exp.component.css']
 })
 export class WorkExpComponent {
 }
