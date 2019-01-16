9cd7a6597f376d2ecd6ec6ea9b2a121af07899a4
Sitthinon Chanaritthichai <31182493+FighttoSitthinon@users.noreply.github.com>
2019-01-16 08:39:25 +0700

    Ubdate about web

--- MyWebApp/ClientApp/src/app/about-web/about-web.component.ts
+++ MyWebApp/ClientApp/src/app/about-web/about-web.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about-web',
   templateUrl: './about-web.component.html',
+  styleUrls: ['./about-web.component.css']
 })
 
 export class AboutComponent {
